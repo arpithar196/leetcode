26869c679d743dbdd257a82b26f1a577fb8a2aca
arpithar196 <136780751+arpithar196@users.noreply.github.com>
2025-04-09 05:13:04 -0700

    Time: 376 ms (13.80%) | Memory: 10.2 MB (11.59%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.c
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.c
@@ -1,10 +1,10 @@
 int firstUniqChar(char* s) {
-    int hash[26]={0};
+    int hash[256]={0};
     for(int i=0;i<strlen(s);i++){
-        hash[s[i]-'a']++;
+        hash[s[i]]++;
     }   
     for(int i=0;i<strlen(s);i++){
-        if(hash[s[i]-'a']==1)
+        if(hash[s[i]]==1)
             return i;
     }
     return -1;
